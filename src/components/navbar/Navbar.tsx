43394673d7a283fb991e6fa36590c4e6f1613b24
Douglas Papani <douglas.papani@hotmail.com>
2024-04-22 13:09:51 -0300

    contruindo a Pagina Home e componentes NavBar e Footer

---
+++ src/components/navbar/Navbar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+
+function Navbar() {
+ 
+  
+
+  return (
+    <>
+     <div className='w-full bg-indigo-900 text-white flex justify-center py-4'>
+          <div className="container flex justify-between text-lg">
+            <div className='text-2xl font-bold uppercase'>Farmacia Generation</div>
+
+            <div className='flex gap-4'>
+              <div className='hover:underline'>Produtos</div>
+              <div className='hover:underline'>Categorias</div>
+              <div className='hover:underline'>Perfil</div>
+              <div className='hover:underline'>Sair</div>
+            </div>
+          </div>
+        </div>
+    </>
+  )
+}
+
+export default Navbar
